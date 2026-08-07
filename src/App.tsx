868698864b8bc4fabc3636dfdc69ea90264d0db0
Rujan <rujanmaharjan194@gmail.com>
2026-08-07 15:29:13 +0545

    feat: implement basic todo list functionality with task addition and display components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Todo from "./components/Todo"
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <Todo />
+    </div>
   )
 }
 
---
+++ src/components/Todo.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import React from "react";
+
+type taskProperties = {
+    id: number;
+    text: string;
+}
+
+function Todo() {
+
+    const [task, setTask] = useState<taskProperties[]>([])
+    const [input, setInput] = useState("")
+
+    const handleTaskInput = () => {
+        if (input === "") {
+            return
+        }
+
+        setTask([...task, {
+            id: Date.now(),
+            text: input
+        }]);
+
+        setInput("");
+    }
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            handleTaskInput()
+        }
+    }
+
+
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Tasks to do."
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyDown={handleKeyDown}
+                className=""
+            />
+            <button onClick={handleTaskInput}>Add task</button>
+
+            <ul>
+                {
+                    task.map((task) => (
+                        <li key={task.id}>{task.text}</li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Todo;
